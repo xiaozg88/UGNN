ea5eb75710bcd8a75c56ca29eee730eb14b2b987
xiaozg88 <36942179+xiaozg88@users.noreply.github.com>
2026-06-12 09:26:14 +0800

    Delete UGNN directory

--- UGNN/ugnn/__init__.py
+++
@@ -1,6 +0,0 @@
-from .config import Config, seed_everything, parse_args
-from .models import (
-    EdgeSoftNCSAGE, EdgeSoftNCGCN, SingleChannelSAGE,
-    StochasticGateMLP, NeutralPrior, InformedPrior,
-)
-from .datasets import DataLoader
